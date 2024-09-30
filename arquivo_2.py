38ea3ea0c6ce8f225169e5a3dc9c074ce386040a
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2024-09-30 12:29:08 +0000

    Arquivo 2 update

--- arquivo_2.py
+++ arquivo_2.py
@@ -1,1 +1,17 @@
-print("Ola minhas lindas molieres")
+class Endereco:
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+        
+    def __str__(self) -> str:
+        return (
+            f"\nENDEREÇO"
+            f"\nLogradouro: {self.logradouro}"
+            f"\nNúmero: {self.numero}"
+            f"\nComplemento: {self.complemento}"
+            f"\nCEP: {self.cep}"
+            f"\nCidade: {self.cidade}"
+        )
