a89401cdb3247c1ee639317f72913bdeb96c6c09
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2024-09-30 14:28:01 +0000

    testes falhando

--- atividade/tests/test_medico.py
+++ atividade/tests/test_medico.py
@@ -1,5 +1,4 @@
 import pytest
-
 from atividade.models.endereco import Endereco
 from atividade.models.medico import Medico
 
@@ -8,26 +7,26 @@ def validar_medico():
     Medico("Nome", "Telefone", "Email", "CRM", Endereco("Logradouro", "Numero", "Complemento", "CEP", "Cidade"))
     return Medico
 
-def validar_atributo_nome(validar_medico):
+def test_validar_atributo_nome(validar_medico):
     assert validar_medico.nome == "Nome"
 
-def validar_atributo_telefone(validar_medico):
+def test_validar_atributo_telefone(validar_medico):
     assert validar_medico.telefone == "Telefone"
 
-def validar_atributo_email(validar_medico):
+def test_validar_atributo_email(validar_medico):
     assert validar_medico.crm == "CRM"
 
-def validar_endereco_atributo_logradouro(validar_medico):
+def test_validar_endereco_atributo_logradouro(validar_medico):
     assert validar_medico.endereco.logradouro == "Logradouro"
 
-def validar_endereco_atributo_numero(validar_medico):
+def test_validar_endereco_atributo_numero(validar_medico):
     assert validar_medico.endereco.numero == "Numero"
 
-def validar_endereco_atributo_complemento(validar_medico):
+def test_validar_endereco_atributo_complemento(validar_medico):
     assert validar_medico.endereco.complemento == "Complemento"
 
-def validar_endereco_atributo_cep(validar_medico):
+def test_validar_endereco_atributo_cep(validar_medico):
     assert validar_medico.endereco.cep == "CEP"
 
-def validar_endereco_atributo_cidade(validar_medico):
+def test_validar_endereco_atributo_cidade(validar_medico):
     assert validar_medico.endereco.cidade == "Cidade"
